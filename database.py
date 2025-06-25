9942dee4bbbfeb0b48534ad3448522ee89d5dd85
daniel zhand <dz28.lee20@gmail.com>
2025-06-25 20:16:14 +0000

    beginning conversation from dict db to sqlite

--- database.py
+++ database.py
@@ -1,12 +1,61 @@
+# Script for SQLite3 database creation. Runs on startup
+
 import sqlite3
+from models import Resource, Type
 
-con = sqlite3.connect("paste-link.db")
+# Go back and do error handling for all of methods
 
-cur = con.cursor()  # Create a cursor object to execute SQL commands with Python
 
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS pastes(id, content, vanity_url, type, expiration_time, access_count)"
-)  # Name of the table is pastes
+class DatabaseService:
+    con: sqlite3.Connection
+    cur: sqlite3.Cursor
 
-res = cur.execute("SELECT name FROM sqlite_master")
-print(res.fetchone())
+    def __init__(self, connection: sqlite3.Connection, cursor: sqlite3.Cursor):
+        self.con = connection
+        self.cur = cursor
+
+    def add_entry(self, resource: Resource):
+        self.cur.execute(
+            "INSERT INTO pastes VALUES (?, ?, ?, ?, ?, ?)",
+            (
+                resource.id,
+                resource.content,
+                resource.vanity_url,
+                resource.type.value,
+                resource.expiration_time,
+                resource.access_count,
+            ),
+        )
+        self.con.commit()
+
+    def get_entry(self, id: str) -> Resource:
+        self.cur.execute("SELECT * FROM pastes WHERE id = ?", (id,))
+        return self.tuple_to_resource(self.cur.fetchone())
+
+    def get_all_entries(self):
+        self.cur.execute("SELECT * FROM pastes")
+        return self.cur.fetchall()
+
+    def update_entry(self, id: str, content: str):
+        self.cur.execute(
+            "UPDATE pastes SET content = ? WHERE id = ?",
+            (
+                content,
+                id,
+            ),
+        )
+        self.con.commit()
+
+    def delete_entry(self, id: str):
+        self.cur.execute("DELETE FROM pastes WHERE id = ?", (id,))
+        self.con.commit()
+
+    def tuple_to_resource(self, resource_tuple) -> Resource:
+        return Resource(
+            id=resource_tuple[0],
+            content=resource_tuple[1],
+            vanity_url=resource_tuple[2],
+            type=Type(resource_tuple[3]),
+            expiration_time=resource_tuple[4],
+            access_count=resource_tuple[5],
+        )
---
+++ database_startup.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+con = sqlite3.connect("paste-link.db")
+
+cur = con.cursor()  # Create a cursor object to execute SQL commands with Python
+
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS pastes(id, content, vanity_url, type, expiration_time, access_count)"
+)  # Name of the table is pastes
+
+from database import DatabaseService
+
+db_service = DatabaseService(con, cur)
+
+# cur.execute("SELECT * FROM pastes")
+# print(cur.fetchall())  # Print all entries in the pastes table
+
+# print(type(db_service.get_entry("oqkasl")))  # Print the entry with id "example_id"
+
+# Get list of all ids in the pastes table
+cur.execute("SELECT * FROM pastes WHERE type = ?", ("text",))
+print(cur.fetchall())  # Print all entries in the pastes table
+# Print all ids in the pastes table
