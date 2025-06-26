70ff08d567317d3ab66e15739deba2d842f96749
daniel zhand <dz28.lee20@gmail.com>
2025-06-26 14:58:02 +0000

    database_startup script

--- database_startup.py
+++ database_startup.py
@@ -1,3 +1,5 @@
+# This script initializes a SQLite database for storing paste data. Run on docker container startup
+
 import sqlite3
 
 con = sqlite3.connect("paste-link.db")
@@ -5,20 +7,5 @@ con = sqlite3.connect("paste-link.db")
 cur = con.cursor()  # Create a cursor object to execute SQL commands with Python
 
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS pastes(id, content, vanity_url, type, expiration_time, access_count)"
+    "CREATE TABLE IF NOT EXISTS pastes(id TEXT PRIMARY KEY, content TEXT, vanity_url TEXT, type TEXT, expiration_time INTEGER, access_count INTEGER)"
 )  # Name of the table is pastes
-
-from database import DatabaseService
-
-db_service = DatabaseService(con, cur)
-
-# cur.execute("SELECT * FROM pastes")
-# print(cur.fetchall())  # Print all entries in the pastes table
-
-# print(type(db_service.get_entry("oqkasl")))  # Print the entry with id "example_id"
-
-# Get list of all ids in the pastes table
-cur.execute("SELECT id FROM pastes WHERE type = ?", ("text",))
-# print(cur.fetchall())  # Print all entries in the pastes table
-# Print all ids in the pastes table
-print([row[0] for row in cur.fetchall()])  # Print all ids in the pastes table
