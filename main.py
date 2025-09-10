958e0bcf9104726ac4c2257f685b5e4648ebe768
Daniel Zhang <69740332+D123aniel@users.noreply.github.com>
2025-09-10 14:18:27 +0800

    Cors (#7)
    * Changed expored port to 8000
    
    * removed previous CI yml
    
    * changing CORS, removed creditionals

--- main.py
+++ main.py
@@ -42,7 +42,6 @@ Auth
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins
-    allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
